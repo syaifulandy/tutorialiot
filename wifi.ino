11b621c038a8c7afb7aead03e8e886bf4842c8a7
syaiful andy <syaifulandy@gmail.com>
2018-02-12 07:09:05 +0700

    Update auto reconnect

--- wifi.ino
+++ wifi.ino
@@ -1,27 +1,21 @@
 #include <WiFi.h>
 
-const char* ssid     = "Tulis nama ssid";
-const char* password = "Tulis pass";
+const char* ssid     = "Tulis nama SSID yang digunakan";
+const char* password = "Tulis password SSID";
 
 void setup()
 {
     Serial.begin(115200);
     delay(10);
-
-    // We start by connecting to a WiFi network
-
-    Serial.println();
-    Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
-
     WiFi.begin(ssid, password);
-
+    //Apakah sudah terhubung?
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+      delay(500);
+      Serial.print(".");
+      WiFi.begin(ssid, password);
     }
-
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
@@ -31,6 +25,18 @@ void setup()
 void loop()
 {
     delay(500);
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Reconnecting");
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+          WiFi.begin(ssid, password);
+       }
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
 }
-
 
